fdd68e182f20b552e6b11dd536d85d68c7e91363
egilp04 <egilp04@iesalbarregas.es>
2026-01-09 17:07:28 +0100

    entornos

---
+++ tfg-cuidados/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://gbcmgioreyvuyhxrirgx.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdiY21naW9yZXl2dXloeHJpcmd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE5NTc4NjAsImV4cCI6MjA3NzUzMzg2MH0.nyAQmDfjuxzqdAi2u8PwTv99hrCZ5THcoEP3ek6mReQ',
+};
