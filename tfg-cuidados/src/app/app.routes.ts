bd1d37c9d9581d2f7a0cd9a0d8d626357f9d4072
egilp04 <egilp04@iesalbarregas.es>
2026-01-09 17:05:09 +0100

    configuracin de rutas genericas

--- tfg-cuidados/src/app/app.routes.ts
+++ tfg-cuidados/src/app/app.routes.ts
@@ -1,3 +1,48 @@
 import { Routes } from '@angular/router';
+import { Landing } from './pages/landing/landing';
+import { AboutUs } from './pages/about-us/about-us';
+import { Contact } from './pages/contact/contact';
+import { Manuals } from './pages/manuals/manuals';
+import { Home } from './pages/home/home';
+import { authGuard } from './core/guards/auth-guard';
+import { publicGuard } from './core/guards/public-guard';
+import { ModifyProfilePage } from './pages/modify-profile/modify-profile';
+import { SearchBusiness } from './pages/search-business/search-business';
+import { Activities } from './pages/activities/activities';
+import { Contracts } from './pages/contracts/contracts';
+import { Register } from './pages/register/register';
+import { Notifications } from './pages/notifications/notifications';
+import { Messages } from './pages/messages/messages';
+import { ManagementAdmin } from './pages/management-admin/management-admin';
+import { ManagementTimeGlobal } from './pages/management-time-global/management-time-global';
+import { ManagementServicesGlobal } from './pages/management-services-global/management-services-global';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Servicesbusiness } from './pages/servicesbusiness/servicesbusiness';
+import { Terms } from './pages/terms/terms';
+export const routes: Routes = [
+  { path: '', component: Landing, pathMatch: 'full', canActivate: [publicGuard] },
+  { path: 'about-us', component: AboutUs },
+  { path: 'contact', component: Contact },
+  { path: 'manuals', component: Manuals },
+  { path: 'register', component: Register, runGuardsAndResolvers: 'always' },
+  { path: 'modify-profile', component: ModifyProfilePage, canActivate: [authGuard] },
+  { path: 'home', component: Home, canActivate: [authGuard] },
+  { path: 'contract', component: Contracts, canActivate: [authGuard] },
+  { path: 'activities', component: Activities, canActivate: [authGuard] },
+  { path: 'messages', component: Messages, canActivate: [authGuard] },
+  { path: 'notifications', component: Notifications, canActivate: [authGuard] },
+  { path: 'search-business', component: SearchBusiness, canActivate: [authGuard] },
+  { path: 'admin-gestion', component: ManagementAdmin, canActivate: [authGuard] },
+  { path: 'global-services', component: ManagementServicesGlobal, canActivate: [authGuard] },
+  { path: 'global-times', component: ManagementTimeGlobal, canActivate: [authGuard] },
+  { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
+  { path: 'admin-services', component: Servicesbusiness, canActivate: [authGuard] },
+  { path: 'terms', component: Terms },
 
-export const routes: Routes = [];
+  {
+    path: 'recover-password',
+    loadComponent: () =>
+      import('./pages/recover-password/recover-password').then((m) => m.RecoverPasswordPage),
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
--- tfg-cuidados/src/app/app.ts
+++ tfg-cuidados/src/app/app.ts
@@ -1,12 +1,30 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { Footer } from './components/footer/footer';
+import { Navbar } from './components/navbar/navbar';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer, Navbar],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('tfg-cuidados');
+  protected readonly title = signal('tfg_app');
+  public router = inject(Router);
+  private translate = inject(TranslateService);
+
+  constructor() {
+    this.translate.setDefaultLang('es');
+    this.translate.addLangs(['es', 'en']);
+    const lenguajeGuardado = localStorage.getItem('idioma_seleccionado');
+    if (lenguajeGuardado) {
+      this.translate.use(lenguajeGuardado);
+    } else {
+      const browserLang = this.translate.getBrowserLang();
+      const idiomaInicial = browserLang?.match(/en|es/) ? browserLang : 'es';
+      this.translate.use(idiomaInicial);
+    }
+  }
 }
