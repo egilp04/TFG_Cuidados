e6f0655461abccefc81fa3b100d924a2497b926c
egilp04 <egilp04@iesalbarregas.es>
2026-03-23 15:35:28 +0100

    creacion del chat componente

---
+++ tfg-cuidados/src/app/components/chat-admin/chat-admin.component.cy.ts
@@ -0,0 +1,7 @@
+import { ChatAdminComponent } from './chat-admin.component'
+
+describe('ChatAdminComponent', () => {
+  it('should mount', () => {
+    cy.mount(ChatAdminComponent)
+  })
+})
---
+++ tfg-cuidados/src/app/components/chat-admin/chat-admin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-admin',
+  imports: [],
+  templateUrl: './chat-admin.component.html',
+  styleUrl: './chat-admin.component.css',
+})
+export class ChatAdminComponent {
+
+}
