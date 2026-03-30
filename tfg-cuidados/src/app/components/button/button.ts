6ff6bb3b09625b0bb2f439b7027abb8cee0c2e5f
egilp04 <egilp04@iesalbarregas.es>
2026-03-30 13:22:39 +0200

    video mnual usuario añadido

--- tfg-cuidados/src/app/components/button/button.ts
+++ tfg-cuidados/src/app/components/button/button.ts
@@ -17,15 +17,12 @@ export class ButtonComponent {
   onClick = output<MouseEvent>();
 
   buttonClass = computed(() => {
-    const base =
-      'text-button rounded-12 transition-all duration-200 cursor-pointer flex items-center justify-center border-2 border-transparent px-10 py-6 md:px-15 md:py-8 lg:px-20 lg:py-10 disabled:bg-disabled h-full w-full ';
-
     const variants = {
       primary: `bg-secondary text-surface hover:bg-accent btn-disabled-primary`,
       secondary: `bg-transparent text-primary underline hover:text-accent btn-disabled-secondary`,
       danger: `bg-red text-surface btn-disabled-primary`,
     };
 
-    return `${base} ${variants[this.variant()]}`;
+    return `button-base ${variants[this.variant()]}`;
   });
 }
--- tfg-cuidados/src/app/pages/manuals/manuals.ts
+++ tfg-cuidados/src/app/pages/manuals/manuals.ts
@@ -6,15 +6,17 @@ import { ButtonComponent } from '../../components/button/button';
 import { Buttonback } from '../../components/buttonback/buttonback';
 import { ChatSoporteComponent } from '../../components/chat-soporte/chat-soporte.component';
 import { AuthService } from '../../services/auth.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-manuals',
   standalone: true,
-  imports: [CardsLanding, ButtonComponent, Buttonback, ChatSoporteComponent],
+  imports: [CardsLanding, ButtonComponent, Buttonback, ChatSoporteComponent, TranslateModule],
   templateUrl: './manuals.html',
   styleUrl: './manuals.css',
 })
 export class Manuals {
   public cardsdata: Card[] = cardsdata;
   public authService = inject(AuthService);
+  rol = this.authService.userRol();
 }
