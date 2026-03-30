ae14d523599b7904877991c4ca4e85b6223fe079
egilp04 <egilp04@iesalbarregas.es>
2026-03-30 12:33:19 +0200

    cambios en manuales, si esta autenticado se puede abrir el chat de soporte

--- tfg-cuidados/src/app/pages/manuals/manuals.ts
+++ tfg-cuidados/src/app/pages/manuals/manuals.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CardsLanding } from '../../components/cards-landing/cards-landing';
 import cardsdata from '../../../assets/data/Manuals.json';
 import { Card } from '../../interfaces/card';
 import { ButtonComponent } from '../../components/button/button';
 import { Buttonback } from '../../components/buttonback/buttonback';
 import { ChatSoporteComponent } from '../../components/chat-soporte/chat-soporte.component';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-manuals',
@@ -15,4 +16,5 @@ import { ChatSoporteComponent } from '../../components/chat-soporte/chat-soporte
 })
 export class Manuals {
   public cardsdata: Card[] = cardsdata;
+  public authService = inject(AuthService);
 }
