849bd7ca5effba55bb9692a91294e8a7fdd11230
egilp04 <egilp04@iesalbarregas.es>
2026-04-09 15:48:08 +0200

    cambios en las cards

--- tfg-cuidados/src/app/components/cards-landing/cards-landing.ts
+++ tfg-cuidados/src/app/components/cards-landing/cards-landing.ts
@@ -1,10 +1,9 @@
 import { Component, computed, input } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
-import { NgOptimizedImage } from '@angular/common';
 @Component({
   selector: 'app-cards-landing',
   standalone: true,
-  imports: [TranslateModule, NgOptimizedImage],
+  imports: [TranslateModule],
   templateUrl: './cards-landing.html',
   styleUrl: './cards-landing.css',
 })
@@ -13,9 +12,4 @@ export class CardsLanding {
   descripcion = input.required<string>();
   imagen = input.required<string>();
   cargo = input<string>();
-  public cardClasses = computed(() => {
-    const base = 'p-20 shadow-soft flex flex-col flex flex-col gap-2 min-h-40 lg:min-h-48 w-full';
-    const cargo = this.cargo() ? 'lg:h-64' : 'xl:h-48';
-    return `${base} ${cargo}`;
-  });
 }
