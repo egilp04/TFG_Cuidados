795086e33f3fac7468d602490c6aa5550f4a38d0
egilp04 <egilp04@iesalbarregas.es>
2026-04-09 12:42:59 +0200

    ruta recover psswd

--- tfg-cuidados/src/app/app.routes.ts
+++ tfg-cuidados/src/app/app.routes.ts
@@ -3,96 +3,96 @@ import { authGuard } from './core/guards/auth-guard';
 import { publicGuard } from './core/guards/public-guard';
 
 export const routes: Routes = [
-  { 
-    path: '', 
-    loadComponent: () => import('./pages/landing/landing'), 
-    pathMatch: 'full', 
-    canActivate: [publicGuard] 
+  {
+    path: '',
+    loadComponent: () => import('./pages/landing/landing'),
+    pathMatch: 'full',
+    canActivate: [publicGuard]
   },
   {
     path: 'about-us',
     loadComponent: () => import('./pages/about-us/about-us')
-  }, 
+  },
   {
     path: 'contact',
     loadComponent: () => import('./pages/contact/contact')
-  }, 
-  { 
-    path: 'manuals', 
-    loadComponent: () => import('./pages/manuals/manuals') 
   },
-  { 
-    path: 'register', 
-    loadComponent: () => import('./pages/register/register'), 
-    runGuardsAndResolvers: 'always' 
+  {
+    path: 'manuals',
+    loadComponent: () => import('./pages/manuals/manuals')
   },
-  { 
-    path: 'modify-profile', 
-    loadComponent: () => import('./pages/modify-profile/modify-profile'), 
-    canActivate: [authGuard] 
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register'),
+    runGuardsAndResolvers: 'always'
   },
-  { 
-    path: 'home', 
-    loadComponent: () => import('./pages/home/home'), 
-    canActivate: [authGuard] 
+  {
+    path: 'modify-profile',
+    loadComponent: () => import('./pages/modify-profile/modify-profile'),
+    canActivate: [authGuard]
   },
-  { 
-    path: 'contract', 
-    loadComponent: () => import('./pages/contracts/contracts'), 
-    canActivate: [authGuard] 
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home'),
+    canActivate: [authGuard]
   },
-  { 
-    path: 'activities', 
-    loadComponent: () => import('./pages/activities/activities'), 
-    canActivate: [authGuard] 
+  {
+    path: 'contract',
+    loadComponent: () => import('./pages/contracts/contracts'),
+    canActivate: [authGuard]
   },
-  { 
-    path: 'messages', 
-    loadComponent: () => import('./pages/messages/messages'), 
-    canActivate: [authGuard] 
+  {
+    path: 'activities',
+    loadComponent: () => import('./pages/activities/activities'),
+    canActivate: [authGuard]
   },
-  { 
-    path: 'notifications', 
-    loadComponent: () => import('./pages/notifications/notifications'), 
-    canActivate: [authGuard] 
+  {
+    path: 'messages',
+    loadComponent: () => import('./pages/messages/messages'),
+    canActivate: [authGuard]
   },
-  { 
-    path: 'search-business', 
-    loadComponent: () => import('./pages/search-business/search-business'), 
-    canActivate: [authGuard] 
+  {
+    path: 'notifications',
+    loadComponent: () => import('./pages/notifications/notifications'),
+    canActivate: [authGuard]
   },
-  { 
-    path: 'admin-gestion', 
-    loadComponent: () => import('./pages/management-admin/management-admin'), 
-    canActivate: [authGuard] 
+  {
+    path: 'search-business',
+    loadComponent: () => import('./pages/search-business/search-business'),
+    canActivate: [authGuard]
   },
-  { 
-    path: 'global-services', 
-    loadComponent: () => import('./pages/management-services-global/management-services-global'), 
-    canActivate: [authGuard] 
+  {
+    path: 'admin-gestion',
+    loadComponent: () => import('./pages/management-admin/management-admin'),
+    canActivate: [authGuard]
   },
-  { 
-    path: 'global-times', 
-    loadComponent: () => import('./pages/management-time-global/management-time-global'), 
-    canActivate: [authGuard] 
+  {
+    path: 'global-services',
+    loadComponent: () => import('./pages/management-services-global/management-services-global'),
+    canActivate: [authGuard]
   },
-  { 
-    path: 'dashboard', 
-    loadComponent: () => import('./pages/dashboard/dashboard'), 
-    canActivate: [authGuard] 
+  {
+    path: 'global-times',
+    loadComponent: () => import('./pages/management-time-global/management-time-global'),
+    canActivate: [authGuard]
   },
-  { 
-    path: 'admin-services', 
-    loadComponent: () => import('./pages/servicesbusiness/servicesbusiness'), 
-    canActivate: [authGuard] 
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./pages/dashboard/dashboard'),
+    canActivate: [authGuard]
   },
-  { 
-    path: 'terms', 
-    loadComponent: () => import('./pages/terms/terms') 
+  {
+    path: 'admin-services',
+    loadComponent: () => import('./pages/servicesbusiness/servicesbusiness'),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'terms',
+    loadComponent: () => import('./pages/terms/terms')
   },
   {
     path: 'recover-password',
-    loadComponent: () => import('./pages/recover-password/recover-password').then((m) => m.RecoverPasswordPage),
+    loadComponent: () => import('./pages/recover-password/recover-password')
   },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
