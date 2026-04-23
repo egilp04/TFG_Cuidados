85b07d74656ea04cae69a1eb11f3b8ddc2b2ca6c
egilp04 <egilp04@iesalbarregas.es>
2026-04-23 11:13:39 +0200

    solucion errores estilo forzoso

--- tfg-cuidados/src/app/components/global-notifications/global-notifications.component.ts
+++ tfg-cuidados/src/app/components/global-notifications/global-notifications.component.ts
@@ -11,18 +11,20 @@ import { Component, ElementRef, OnInit, OnDestroy, inject } from '@angular/core'
 })
 export class GlobalNotificationsComponent implements OnInit, OnDestroy {
   public messageService = inject(MessageService);
-  constructor(private el: ElementRef) {}
-  ngOnInit() {
-    document.body.appendChild(this.el.nativeElement);
+  constructor(private el: ElementRef) { }
 
-    this.el.style.position = 'fixed';
-    this.el.style.top = '0';
-    this.el.style.left = '0';
-    this.el.style.width = '100%';
-    this.el.style.zIndex = '999999';
-    this.el.style.pointerEvents = 'none';
+  ngOnInit() {
+
+    const elementoHTML = this.el.nativeElement;
+    document.body.appendChild(elementoHTML);
+
+    elementoHTML.style.top = '0';
+    elementoHTML.style.left = '0';
+    elementoHTML.style.width = '100%';
+    elementoHTML.style.zIndex = '999999';
+    elementoHTML.style.pointerEvents = 'none';
   }
   ngOnDestroy() {
     this.el.nativeElement.remove();
-}
+  }
 }
