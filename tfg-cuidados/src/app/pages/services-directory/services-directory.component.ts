b0cb54c64a54b02eedc0d08505a033914bd778ce
egilp04 <egilp04@iesalbarregas.es>
2026-04-30 09:32:56 +0200

    ruta corregida con core para la importacino de iconos. Directorio de servicios con iconos

--- tfg-cuidados/src/app/pages/services-directory/services-directory.component.ts
+++ tfg-cuidados/src/app/pages/services-directory/services-directory.component.ts
@@ -7,6 +7,9 @@ import { TranslateModule } from '@ngx-translate/core';
 import { ButtonComponent } from '../../components/button/button';
 import { tap, catchError, of } from 'rxjs';
 import { CardSkeletonComponent } from '../../components/card-skeleton/card-skeleton.component';
+import { AVAILABLE_ICONS, SERVICE_ICON_MAP } from '../../core/utils/icons';
+import { LucideAngularModule } from 'lucide-angular';
+
 /**
  * Componente para mostrar el directorio de servicios disponibles.
  * Permite a los usuarios navegar a negocios que ofrecen un servicio específico.
@@ -14,13 +17,15 @@ import { CardSkeletonComponent } from '../../components/card-skeleton/card-skele
 @Component({
   selector: 'app-services-directory',
   standalone: true,
-  imports: [TranslateModule, ButtonComponent, CardSkeletonComponent],
+  imports: [TranslateModule, ButtonComponent, CardSkeletonComponent, LucideAngularModule],
   templateUrl: './services-directory.component.html',
   styleUrl: './services-directory.component.css',
 })
 export default class ServicesDirectoryComponent {
   private serviceService = inject(ServiceService);
   private router = inject(Router);
+
+  public iconMap = SERVICE_ICON_MAP;
 
   public allServices = toSignal(
     this.serviceService.getServicesWithOffers(),
