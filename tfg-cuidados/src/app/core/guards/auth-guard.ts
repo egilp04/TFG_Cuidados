7721aa85f857918a15ab1d8a14c2c639e47fec51
egilp04 <egilp04@iesalbarregas.es>
2026-05-23 18:44:15 +0200

    proteccion de rutas

--- tfg-cuidados/src/app/core/guards/auth-guard.ts
+++ tfg-cuidados/src/app/core/guards/auth-guard.ts
@@ -7,14 +7,22 @@ import { filter, map, take } from 'rxjs';
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
+
   return toObservable(authService.isLoading).pipe(
     filter((loading) => !loading),
     take(1),
     map(() => {
-      if (authService.isAuthenticated()) {
-        return true;
+      if (!authService.isAuthenticated()) {
+        return router.createUrlTree(['/']);
       }
-      return router.createUrlTree(['/']);
-    })
+      const allowedRoles = route.data['roles'] as Array<string>;
+      if (allowedRoles) {
+        const currentUserRole = authService.userRol() ?? '';
+        if (!allowedRoles.includes(currentUserRole)) {
+          return router.createUrlTree(['/landing']);
+        }
+      }
+      return true;
+    }),
   );
 };
